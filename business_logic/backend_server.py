d5755fdf6ae7aa2c93e44c3d4844ff4826391b0a
Octavian Torcea <octavian.torcea@YAHOO.COM>
2024-01-10 01:38:17 +0200

    added server starting method with basic testing method

---
+++ business_logic/backend_server.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+DEFAULT_HOST = "0.0.0.0"
+DEFAULT_PORT = 12345
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+if __name__ == "__main__":
+    app.run(host=DEFAULT_HOST, port=DEFAULT_PORT, debug=True)
