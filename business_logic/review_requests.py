70dae5dd02d0891b6372d4bd6a68a89abc178189
octavian <octavian.torcea@yahoo.com>
2024-01-11 19:56:05 +0200

    updated review requests

--- business_logic/review_requests.py
+++ business_logic/review_requests.py
@@ -37,15 +37,40 @@ def movie_score():
     body = request.get_json()
 
     if request.method == POST:
-        if collections.Counter(body.keys()) != collections.Counter(SCORE_POST_REQ_KEYS):
+        try:
+            id_user = body["id_user"]
+            id_movie = body["id_movie"]
+            score = body["score"]
+        except Exception as _:
             return Response(status=HTTPStatus.BAD_REQUEST)
 
-        return f"SCORE POST with: {body}"
+        query = (f"INSERT INTO SCORES (score, id_movie, id_user) "
+                 f"VALUES ({score}, {id_movie}, {id_user})")
+
+        try:
+            cursor.execute(query)
+        except Exception as e:
+            return Response(status=409, response=str(e))
+
+        db.commit()
+        return json.dumps({'id': cursor.lastrowid}), 201
+
     elif request.method == DELETE:
-        if collections.Counter(body.keys()) != collections.Counter(SCORE_DEL_REQ_KEYS):
+        try:
+            id_score = body["id_score"]
+        except Exception as _:
             return Response(status=HTTPStatus.BAD_REQUEST)
 
-        return f"SCORE DEL with: {body}"
+        query = f"DELETE FROM SCORES WHERE id = {id_score}"
+
+        try:
+            cursor.execute(query)
+        except Exception as e:
+            return Response(status=404, response=str(e))
+
+        db.commit()
+        return Response(status=200)
+
     else:
         return Response(status=HTTPStatus.METHOD_NOT_ALLOWED)
 
@@ -55,15 +80,41 @@ def movie_comment():
     body = request.get_json()
 
     if request.method == POST:
-        if collections.Counter(body.keys()) != collections.Counter(COMMENT_POST_REQ_KEYS):
+        try:
+            id_user = body["id_user"]
+            id_movie = body["id_movie"]
+            comment = body["comment"]
+        except Exception as _:
             return Response(status=HTTPStatus.BAD_REQUEST)
 
-        return f"COMMENT POST with: {body}"
+        query = (f"INSERT INTO COMMENTS (comment, id_user, id_movie) "
+                 f"VALUES (%s, %s, %s)")
+        values = (comment, id_user, id_movie)
+
+        try:
+            cursor.execute(query, values)
+        except Exception as e:
+            return Response(status=409, response=str(e))
+
+        db.commit()
+        return json.dumps({'id': cursor.lastrowid}), 201
+
     elif request.method == DELETE:
-        if collections.Counter(body.keys()) != collections.Counter(COMMENT_DEL_REQ_KEYS):
+        try:
+            id_comment = body["id_comment"]
+        except Exception as _:
             return Response(status=HTTPStatus.BAD_REQUEST)
 
-        return f"COMMENT DEL with: {body}"
+        query = f"DELETE FROM COMMENTS WHERE id = {id_comment}"
+
+        try:
+            cursor.execute(query)
+        except Exception as e:
+            return Response(status=404, response=str(e))
+
+        db.commit()
+        return Response(status=200)
+
     else:
         return Response(status=HTTPStatus.METHOD_NOT_ALLOWED)
     
